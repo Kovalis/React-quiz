a6b1c82f1791bfcd0c15cd6c07972bcd1298c4fe
Igor <kovalis@grechka.digital>
2022-07-20 13:25:01 +0300

    init

---
+++ src/containers/QuizList/QuizList.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import classes from "./QuizList.module.css";
+import { Link } from "react-router-dom";
+import Loader from "../../components/UI/Loader/Loader";
+import axios from "../../axios/axios-quiz";
+
+export default class QuizList extends Component {
+  state = {
+    quizes: [],
+    loading: true,
+  };
+
+  renderQuizes() {
+    return this.state.quizes.map((quiz) => {
+      return (
+        <li key={quiz.id}>
+          <Link to={"/quiz/" + quiz.id}>{quiz.name}</Link>
+        </li>
+      );
+    });
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get("/quizes.json");
+
+      const quizes = [];
+
+      Object.keys(response.data).forEach((key, index) => {
+        quizes.push({
+          id: key,
+          name: `Текст №${index + 1}`,
+        });
+      });
+
+      this.setState({
+        quizes: quizes,
+        loading: false,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <div className={classes.QuizList}>
+        <h1>Список тестов</h1>
+
+        {this.state.loading ? <Loader /> : <ul>{this.renderQuizes()}</ul>}
+      </div>
+    );
+  }
+}
